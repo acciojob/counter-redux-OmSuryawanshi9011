5f6d2b5650a1c95941335c1017bc7e6852aaee48
OmSuryawanshi9011 <omsuryawanshi9011@gmail.com>
2025-06-23 17:15:48 +0530

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useSelector, useDispatch } from "react-redux";
+import { increment,decrement } from "../state/counterSlice";
 const App = () => {
+
+  const count = useSelector((state) => state.counter.value)
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div>
+          <h1>{count}</h1>
+        </div>
+        <div>
+          <button>Increment By One</button>
+        </div>
+        <div>
+          <button>Decrement By One</button>
+        </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import store from "./store";
+import { Provider } from "react-redux";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            
+        </Provider>
+    </React.StrictMode>
+)
